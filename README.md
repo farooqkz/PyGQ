01789af7a1f15291e15a80b171e46a9bf0bc2923
FarooqKZ <farooghkarimizadeh@gmail.com>
2018-03-28 19:05:02 +0430

    Added pygq.py and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PyGQ
-A python module to get Quran verses using GlobalQuran API
+### A python module to get Quran verses using GlobalQuran API
+
+PyGQ uses GlobalQuran API to get Quran verses in multiple translations. It was
+a part of KoranFinder after I decided to re-write this part and now it can be
+used as an independent module.
+
+## Usage
+This is a sample code with PyGQ which gets the first ayah of the first surah:
+```
+import pygq
+Q = pygq.PyGQ()
+print(Q.getAyah(1, 1, "en.sahih")["verse"])
+```
+
+_Note that getAyah() returns a Dict containing the ayah text, number, surah
+number and id._
+
+## Licence
+see ISC.
+
---
+++ pygq.py
@@ -0,0 +1,65 @@
+"""
+    Copyright 2018 Farooq Karimi Zadeh <farooghkarimizadeh at gmail dot com>
+
+"""
+
+import requests
+
+class QData:
+    SURAH_LAST = 114
+    SURAH_FIRST = 1
+    def __init__(self, url="http://api.globalquran.com/ayah/", token="", lg_codes={}):
+        """url and token are API url and token. lg_codes is a dict which maps 
+           2 letter language codes to their fullname. like:
+           {"en": "en.sahih", "ar": "quran-simple"}
+        """
+
+        self.url = url
+        self.token = token
+        self.lg_codes = lg_codes
+
+    def getAyah(self, surah, ayah, lang):
+        """Returns a dict containing the verse itself, ayah and surah number
+        and id. example:
+            {
+            "surah": 1,
+            "ayah": 1,
+            "id": 1,
+            "verse": "In the name of Allah, the Entirely Merciful, the Especially Merciful."
+            }
+        """
+        if len(lang) == 2:
+            if lang in self.lg_codes:
+                lang = self.lg_codes[lang]
+            else:
+                raise ValueError(lang+ " is not supported using 2letter codes")
+        
+        if (not self.SURAH_FIRST <= surah <= self.SURAH_LAST):
+            raise ValueError("surah(chapter) must be between "
+                             + str(self.SURAH_FIRST) + " and " +
+                             str(self.SURAH_LAST))
+
+        req_url = self.url + str(surah) + ":" + str(ayah) + "/" + lang
+        ayah_json = requests.get(req_url, params = {'key': self.token}).json()
+        while len(ayah_json) == 1:
+            ayah_json = ayah_json[next(iter(ayah_json))]
+
+        if ayah_json["surah"] != surah:
+            raise ValueError("Invalid ayah number for this surah")
+        
+        return ayah_json
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], "surah:ayah [lang]")
+        sys.exit(0)
+    
+    surah, ayah = sys.argv[1].split(":")
+    surah = int(surah)
+    ayah = int(surah)
+    lang = "en.sahih" if len(sys.argv) < 3 else sys.argv[2]
+
+    Q = QData()
+    print(Q.getAyah(surah, ayah, lang)["verse"])
+    
